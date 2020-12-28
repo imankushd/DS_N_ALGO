65aea4806f8b11704255c7d662ce223da145ce70
imankushd <philic1412@gmail.com>
2020-12-28 17:30:47 +0530

    Add files via upload

---
+++ src/main/java/com/dsAlgo/DFSTree.java
@@ -0,0 +1,42 @@
+package com.dsAlgo.tree;
+
+class BSTNode
+{
+	int data;
+	BSTNode left;
+	BSTNode right;
+	
+	BSTNode(int data)
+	{
+		this.data=data;
+	}
+}
+
+public class DFSTree {
+
+	static BSTNode root=null;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    root = new BSTNode(1);
+    root.left = new BSTNode(2);
+    root.right = new BSTNode(3);
+    root.left.left = new BSTNode(4);
+    root.left.right = new BSTNode(5);
+    root.right.left = new BSTNode(6);
+    root.right.right = new BSTNode(7);
+    
+    dfs(root);
+	}
+
+	private static void dfs(BSTNode root) {
+
+		if(root==null)
+			return;
+		
+		System.out.print(root.data+" ");
+		dfs(root.left);
+		dfs(root.right);
+	}
+
+}
